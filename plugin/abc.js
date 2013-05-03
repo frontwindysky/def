46c83dc308f2c687dacec4f2dfd337d36ede956e
yuchun <yuchun@taobao.com>
2013-05-03 16:37:31 +0800

    plugin solution

---
+++ plugin/abc.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name : "abc",		//插件名称
+	description : "构建工具，包括编译，打包，压缩",	//插件描述
+	port : "8001",			//插件启用端口
+	setting : "/setting",
+	use : "/tasking",
+	start : function(){}		//插件启动方法
+}
--- plugin/vmarket.js
+++ plugin/vmarket.js
@@ -1,8 +1,8 @@
 module.exports = {
 	name : "vmarket",		//插件名称
 	description : "vm渲染引擎",	//插件描述
-	portocol 
-	action : "demo",			//插件操作目录，或assets
-	port : "8001",			//插件启用端口
-	start : function(){}		//插件启动方法
+	port : "8002",			//插件启用端口
+	setting : "/setting",
+	use : "/tasking",
+	start : "vm"		//插件启动方法
 }
