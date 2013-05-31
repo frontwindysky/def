cdded540aa9a10c6d63b1df36a79f0115c2bec4d
yuchun <yuchun@taobao.com>
2013-05-31 10:59:36 +0800

    plugin

--- plugin/vmarket.js
+++ plugin/vmarket.js
@@ -1,8 +1,7 @@
 module.exports = {
 	name : "vmarket",		//插件名称
 	description : "vm渲染引擎",	//插件描述
-	port : "8002",			//插件启用端口
-	settingPage : "/setting",
+	settingPage : "/",
 	workingPage : "/tasking",
-	start : "vm"		//插件启动方法
+	sever : "vm -p <%= port %>"		//插件启动方法
 }
