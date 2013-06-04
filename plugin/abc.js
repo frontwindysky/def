54133ba95f6aa7e8cb5827260a658bcd51d8ce80
yuchun <yuchun@taobao.com>
2013-06-04 09:44:35 +0800

    plugin

--- plugin/abc.js
+++ plugin/abc.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name : "abc",		//插件名称 
 	description : "构建工具，包括编译，打包，压缩",	//插件描述
-	server : "abc -p <%= port %>",
+	start : "abc -p <%= port %>",
 	settingPage : "/setting",
 	workingPage : "/working"
 	
--- plugin/vmarket.js
+++ plugin/vmarket.js
@@ -2,6 +2,6 @@ module.exports = {
 	name : "vmarket",		//插件名称
 	description : "vm渲染引擎",	//插件描述
 	settingPage : "/",
-	workingPage : "/tasking",
-	sever : "vm -p <%= port %>"		//插件启动方法
+	workingPage : "/list/TXM_K_forward_201211060",
+	start : "vm -p <%= port %>"		//插件启动方法
 }
