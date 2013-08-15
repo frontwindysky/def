efde216943c5d44ac05405d5525297c05f9a9771
yuchun <yuchun@taobao.com>
2013-08-15 16:22:01 +0800

    proxy->httpx

--- plugin/proxy.js
+++ plugin/proxy.js
@@ -1,7 +1,7 @@
 module.exports = {
-	name : "Proxy",		//插件名称 
-	npm : "magic-cube",	//npm名称	
+	name : "HttpX",		//插件名称 
+	npm : "httpx",	//npm名称	
 	description : "Assets代理",	//插件描述
-	start : "mc -p 8060",
+	start : "tt -p 8060",
 	port : "8060"
 }
